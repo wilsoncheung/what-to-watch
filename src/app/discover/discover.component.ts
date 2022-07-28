ae3b02b0c88e125d06470d660960cac6e6f096a4
wilsoncheung <cheungwilson.edu@gmail.com>
2022-07-28 18:39:42 -0400

    Added Discover component mock up

--- src/app/discover/discover.component.ts
+++ src/app/discover/discover.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { MoviesService } from '../core/services/movies.service'
+import { MoviesService } from '../core/services/movies.service';
+import { environment } from '../../environments/environment';
+import { faFilm } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'discover',
@@ -7,12 +9,20 @@ import { MoviesService } from '../core/services/movies.service'
   styleUrls: ['./discover.component.css']
 })
 export class DiscoverComponent implements OnInit {
+  faFilm = faFilm;
+  public env: any = environment;
+  public movies: any;
 
-  constructor(private movies: MoviesService) { }
+  constructor(private moviesService: MoviesService) { }
 
   ngOnInit(): void {
-    this.movies.discover().subscribe((res: any) => {
-      console.log("DiscoverAPI Called:", res);
+
+    this.moviesService.discover().subscribe((res: any) => {
+      if (res != null) {
+        console.log("DiscoverAPI Called:", res);
+        this.movies = res.results.slice(0, 6); // show 6 for now, implement pagination...
+        //this.movies = res.results;
+      }
     });
   }
 

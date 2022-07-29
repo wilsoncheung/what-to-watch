5c579bc23ea7961f8bddf8da8468e2432e1ca624
wilsoncheung <cheungwilson.edu@gmail.com>
2022-07-29 18:35:27 -0400

    Included ng-bootstrap for typeahead & UI changes for swiper in discover component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from "@angular/forms";
 import { MoviesService } from '../app/core/services/movies.service';
 
 import { AppComponent } from './app.component';
@@ -19,7 +21,9 @@ import { SearchComponent } from './search/search.component';
     BrowserModule,
     HttpClientModule,
     FontAwesomeModule,
-    NgxUsefulSwiperModule
+    FormsModule,
+    NgxUsefulSwiperModule,
+    NgbModule
   ],
   providers: [
     Title,

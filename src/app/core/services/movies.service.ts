145c86efd6841b1b7029b45bd2eb536c86f580f4
wilsoncheung <cheungwilson.edu@gmail.com>
2022-10-18 12:55:09 -0400

    Added movie service for handling of API calls

--- src/app/core/services/movies.service.ts
+++ src/app/core/services/movies.service.ts
@@ -12,48 +12,66 @@ export class MoviesService {
     .set('Content-Type', 'application/json')
     .set('api-key', environment.apiKey);
 
+  private httpOptions = { headers: this._headers };
+
   constructor(private http: HttpClient) { }
 
-  public discover() {
-    let headers = this._headers;
+  public getYoutubeUrl(key: string) {
+    return "https://www.youtube.com/embed/" + key;
+  }
 
-    return this.http.get(`${environment.baseURL}discover/movie?api_key=${environment.apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`, { headers });
+  public discover() {
+    //let headers = this._headers;
+
+    return this.http.get(`${environment.baseURL}discover/movie?api_key=${environment.apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`, this.httpOptions);
   }
 
   public genres() {
-    let headers = this._headers;
+    //let headers = this._headers;
 
-    return this.http.get(`${environment.baseURL}genre/movie/list?api_key=${environment.apiKey}&language=en-US`, { headers });
+    return this.http.get(`${environment.baseURL}genre/movie/list?api_key=${environment.apiKey}&language=en-US`, this.httpOptions);
   }
 
   public search(query: string) {
-    let headers = this._headers;
+    //let headers = this._headers;
 
     if (query === '') {
       return of([]);
     }
 
-    return this.http.get<any>(`${environment.baseURL}search/movie?api_key=${environment.apiKey}&language=en-US&query=${query}&page=1&include_adult=false`, { headers })
+    return this.http.get<any>(`${environment.baseURL}search/movie?api_key=${environment.apiKey}&language=en-US&query=${query}&page=1&include_adult=false`, this.httpOptions)
       .pipe(map(response => response.results));
   }
 
   public movieDetails(id: number) {
-    let headers = this._headers;
+    //let headers = this._headers;
 
     if (typeof id != "number") {
       return of([]);
     }
 
-    return this.http.get(`${environment.baseURL}movie/${id}?api_key=${environment.apiKey}&language=en-US&append_to_response=videos`, { headers });
+    return this.http.get(`${environment.baseURL}movie/${id}?api_key=${environment.apiKey}&language=en-US&append_to_response=videos`, this.httpOptions);
   }
 
   public recommendations(id: number) {
-    let headers = this._headers;
+    //let headers = this._headers;
 
     if (typeof id != "number") {
       return of([]);
     }
 
-    return this.http.get(`${environment.baseURL}movie/${id}/recommendations?api_key=${environment.apiKey}&language=en-US&page=1`, { headers });
+    return this.http.get<any>(`${environment.baseURL}movie/${id}/recommendations?api_key=${environment.apiKey}&language=en-US&page=1`, this.httpOptions)
+      .pipe(map(resp => resp.results));
+  }
+
+  public trailer(id: number) {
+    //let headers = this._headers;
+
+    if (typeof id != "number") {
+      return of([]);
+    }
+
+    return this.http.get<any>(`${environment.baseURL}movie/${id}/videos?api_key=${environment.apiKey}&language=en-US`, this.httpOptions)
+      .pipe(map(resp => resp.results));
   }
 }

5665fb22d956510b28d02eb8f9543293c1bd89cc
wilsoncheung <cheungwilson.edu@gmail.com>
2022-10-18 12:55:16 -0400

    Misc

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  baseURL: "https://api.themoviedb.org/3/",
+  imgURL: "https://image.tmdb.org/t/p/",          // original(SVG) / w500
+  apiKey: "ef7291a469f1ea67c2f23af1c31deb42"
 };
